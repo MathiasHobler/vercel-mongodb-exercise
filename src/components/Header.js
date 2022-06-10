5c3a6686e752d5ef7326aff848be8f539fa1ba8a
Mathias Hobler <hoblermathias@icloud.com>
2022-06-10 14:54:01 +0200

    create header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Header = () => {
-	return <div>Header</div>;
+const Header = ({titel}) => {
+	return (
+		<HeaderContainer>
+			<h1>{titel}</h1>
+		</HeaderContainer>
+	);
 };
 
 export default Header;
+
+const HeaderContainer = styled.header`
+	background-color: lightcoral;
+	width: 100%;
+	text-align: center;
+
+	h1 {
+		margin: 0;
+		padding: 0.25em;
+	}
+`;
